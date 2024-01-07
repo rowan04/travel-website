83e636d2ef125172d215c2a49cbf69604e12a994
Rowan <rowanmoss04@gmail.com>
2024-01-07 17:05:42 +0000

    Initial page workings and navigation

--- js/map.js
+++ js/map.js
@@ -1,15 +1,20 @@
-// Find the map div
-const map_div = document.getElementById('osm-map');
+function createMap() {
+    // Find the map div
+    const map_div = document.getElementById('osm-map');
 
-// Create a Leaflet map in map_div
-let map = L.map(map_div);
-// Add OSM tile layer to the Leaflet map
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    // Create a Leaflet map in map_div
+    // The below code to create the map was taken from a stack overflow answer at 
+    // https://stackoverflow.com/questions/925164/openstreetmap-embedding-map-in-webpage-like-google-maps
+    // by the user 'totymedli'
+    let map = L.map(map_div);
+    // Add OSM tile layer to the Leaflet map
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-// Set map to centre on a set location, with zoom of 11
-const location1 = L.latLng('51.751887', '-1.257726');
-map.setView(location1, 11);
-// Place a marker on the same location
-L.marker(location1).addTo(map);
+    // Set the map to centre on a set location, with zoom of 11
+    const location1 = L.latLng('51.751887', '-1.257726');
+    map.setView(location1, 11);
+    // Place a marker on the same location
+    L.marker(location1).addTo(map);
+}
